0d4353cd6a3e0680bb4747a9f3bc1243af9d21d7
Moe-Baker <moe4baker@gmail.com>
2020-05-27 12:15:52 +0200

    Work

---
+++ Assets/Objects/Weapons/Module/General/WeaponSandbox.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.AI;
+
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditorInternal;
+#endif
+
+using Object = UnityEngine.Object;
+using Random = UnityEngine.Random;
+
+namespace Game
+{
+	public class WeaponSandbox : Weapon.Module
+	{
+        public override void Init()
+        {
+            base.Init();
+
+            Weapon.OnProcess += Process;
+        }
+
+        void Process(Weapon.IProcessData data)
+        {
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                Weapon.Mesh.Animator.SetTrigger("Jump");
+            }
+        }
+    }
+}
