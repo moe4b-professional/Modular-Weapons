02b9f841b8d0a5bce9a5bf63ee8beaa11dfbc66d
Moe-Baker <moe4baker@gmail.com>
2020-06-06 15:00:53 +0200

    Work

---
+++ Assets/General/Scripts/Utility/CursorControls.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.AI;
+
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditorInternal;
+#endif
+
+using Object = UnityEngine.Object;
+using Random = UnityEngine.Random;
+
+namespace Game
+{
+	public class CursorControls : MonoBehaviour
+	{
+        public KeyCode key = KeyCode.F;
+
+        bool isOn = false;
+
+        private void Start()
+        {
+            UpdateState();
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(key))
+                Toggle();
+        }
+
+        private void Toggle()
+        {
+            isOn = !isOn;
+
+            UpdateState();
+        }
+
+        private void UpdateState()
+        {
+            Cursor.visible = isOn;
+
+            Cursor.lockState = isOn ? CursorLockMode.None : CursorLockMode.Locked;
+        }
+    }
+}
