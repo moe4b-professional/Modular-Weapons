7467ed80c98df7a7faf35b8d37688659f95539a8
Moe-Baker <moe4baker@gmail.com>
2020-06-11 18:58:37 +0200

    Work

---
+++ Assets/General/Scripts/AutoRenderTexture.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.AI;
+
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditorInternal;
+#endif
+
+using Object = UnityEngine.Object;
+using Random = UnityEngine.Random;
+
+namespace Game
+{
+    public class AutoRenderTexture : MonoBehaviour
+	{
+		[SerializeField]
+        protected Camera source;
+
+        [SerializeField]
+        protected Renderer target;
+
+        [SerializeField]
+        protected int width = 512;
+        public int Width { get { return width; } }
+
+        [SerializeField]
+        protected int height = 512;
+        public int Height { get { return height; } }
+
+        public RenderTexture Texture { get; protected set; }
+
+        void Start()
+        {
+            Texture = Create();
+
+            source.targetTexture = Texture;
+
+            target.material.mainTexture = Texture;
+        }
+
+        RenderTexture Create()
+        {
+            var result = new RenderTexture(width, height, 16);
+
+            return result;
+        }
+
+        void OnDestroy()
+        {
+            if (Texture != null && Texture.IsCreated())
+                Texture.Release();
+        }
+    }
+}
---
+++ Assets/General/Scripts/ManualCameraRender.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.AI;
+
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditorInternal;
+#endif
+
+using Object = UnityEngine.Object;
+using Random = UnityEngine.Random;
+
+namespace Game
+{
+    [RequireComponent(typeof(Camera))]
+	public class ManualCameraRender : MonoBehaviour
+	{
+        [SerializeField]
+        protected int _FPS = 60;
+        public int FPS { get { return _FPS; } }
+
+        public float FrameTime => 1f / FPS;
+
+        float timer = 0f;
+
+        Camera component;
+
+        void Awake()
+        {
+            component = GetComponent<Camera>();
+
+            component.enabled = false;
+        }
+
+        void Update()
+        {
+            timer += Time.unscaledDeltaTime;
+
+            if (timer >= FrameTime)
+            {
+                timer = 0f;
+                component.Render();
+            }
+        }
+    }
+}
