99261766a133ca1988adedcac57bc712fb3da4a7
Moe-Baker <moe4baker@gmail.com>
2020-06-17 07:24:56 +0200

    Work

---
+++ Assets/Objects/Weapon/Module/Action/Utility/WeaponActionInputAnimation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.AI;
+
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditorInternal;
+#endif
+
+using Object = UnityEngine.Object;
+using Random = UnityEngine.Random;
+
+namespace Game
+{
+	public class WeaponActionInputAnimation : Weapon.Module
+	{
+        public const string ID = "Input";
+
+        public Animator Animator => Weapon.Mesh.Animator;
+
+        public override void Init()
+        {
+            base.Init();
+
+            Weapon.OnProcess += Process;
+        }
+
+        void Process()
+        {
+            Animator.SetBool(ID, Weapon.Action.Input);
+        }
+    }
+}
