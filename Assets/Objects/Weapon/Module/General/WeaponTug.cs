c10084601ae7d53e9bb8ff485c009a98a6c7bd69
Moe-Baker <moe4baker@gmail.com>
2020-07-16 22:52:35 +0200

    Work

---
+++ Assets/Objects/Weapon/Module/General/WeaponTug.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.AI;
+
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditorInternal;
+#endif
+
+using Object = UnityEngine.Object;
+using Random = UnityEngine.Random;
+
+namespace Game
+{
+	public class WeaponTug : Weapon.Module
+	{
+        [SerializeField]
+        protected float range = 0.004f;
+        public float Range { get { return range; } }
+
+        [SerializeField]
+        protected float speed = 10f;
+        public float Speed { get { return speed; } }
+
+        [SerializeField]
+        protected float max = 3f;
+        public float Max { get { return max; } }
+
+        public float Target { get; protected set; }
+        public float Value { get; protected set; }
+
+        public IProcessor Processor { get; protected set; }
+		public interface IProcessor
+        {
+            Vector3 PlanarVelocity { get; }
+        }
+
+        public WeaponPivot Pivot => Weapon.Pivot;
+        public Transform Context => Pivot.transform;
+
+        public override void Configure()
+        {
+            base.Configure();
+
+            Processor = GetProcessor<IProcessor>();
+        }
+
+        public override void Init()
+        {
+            base.Init();
+
+            Pivot.OnProcess += Process;
+        }
+
+        void Process()
+        {
+            CalculateTarget();
+
+            Value = Mathf.Lerp(Value, Target, speed * Time.deltaTime);
+
+            Context.position -= Weapon.transform.forward * Value;
+        }
+
+        protected virtual void CalculateTarget()
+        {
+            Target = Processor.PlanarVelocity.magnitude;
+
+            Target = Mathf.Clamp(Target, 0f, max);
+
+            Target *= range;
+        }
+    }
+}
