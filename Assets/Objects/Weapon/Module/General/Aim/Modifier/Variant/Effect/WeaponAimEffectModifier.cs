5e5163ebdcbd3d3233994e3cbfc071e72e1ce218
Moe-Baker <moe4baker@gmail.com>
2021-03-18 12:01:54 +0200

    Work

--- Assets/Objects/Weapon/Module/General/Aim/Modifier/Variant/Effect/WeaponAimEffectModifier.cs
+++ Assets/Objects/Weapon/Module/General/Aim/Modifier/Variant/Effect/WeaponAimEffectModifier.cs
@@ -25,59 +25,6 @@ namespace Game
         protected ValueRange range = new ValueRange(0.3f, 1f);
         public ValueRange Range { get { return range; } }
 
-        [SerializeField]
-        protected OverridesProperty overrides;
-        public OverridesProperty Overrides { get { return overrides; } }
-        [Serializable]
-        public class OverridesProperty
-        {
-            [SerializeField]
-            protected List<WeaponAimEffectModifier> list;
-            public List<WeaponAimEffectModifier> List { get { return list; } }
-
-            public WeaponAimEffectModifier Modifier { get; protected set; }
-            public virtual void Configure(WeaponAimEffectModifier reference)
-            {
-                Modifier = reference;
-
-                RegisterChildern();
-            }
-
-            protected virtual void RegisterChildern()
-            {
-                var childern = Dependancy.GetAll<WeaponAimEffectModifier>(Modifier.gameObject, Dependancy.Scope.Childern);
-
-                for (int i = 0; i < childern.Count; i++)
-                {
-                    if (Contains(childern[i])) continue;
-
-                    Add(childern[i]);
-                }
-            }
-
-            public virtual void Add(WeaponAimEffectModifier element)
-            {
-                if (Contains(element))
-                {
-                    Debug.LogWarning("Trying to add duplicate " + typeof(WeaponAimEffectModifier).Name + ": " + element.name);
-                    return;
-                }
-
-                list.Add(element);
-            }
-
-            public virtual bool Contains(WeaponAimEffectModifier target) => list.Contains(target);
-
-            public virtual bool Detect(WeaponEffects.IInterface target)
-            {
-                for (int i = 0; i < list.Count; i++)
-                    if (list[i].IsTarget(target))
-                        return true;
-
-                return false;
-            }
-        }
-
         public List<Element> Elements { get; protected set; }
         [Serializable]
         public class Element
@@ -86,9 +33,7 @@ namespace Game
 
             public WeaponEffects.IInterface Target { get; protected set; }
 
-            public bool Overriden => EffectModifier.Overrides.Detect(Target);
-
-            public float Value => Overriden ? 1f : EffectModifier.Value;
+            public float Value => EffectModifier.Value;
 
             public float Modifier() => Value;
 
@@ -114,8 +59,6 @@ namespace Game
         public override void Configure()
         {
             base.Configure();
-
-            overrides.Configure(this);
 
             Elements = new List<Element>();
 
